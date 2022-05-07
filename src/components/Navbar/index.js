819c993ff201cb71af0a731370cafbd6bf3d3326
Dicky Ardiar <dickyardiar1@gmail.com>
2022-05-07 20:22:53 +0700

    adding component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import data from './data';
+import { useHistory } from 'react-router-dom';
 
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
@@ -34,6 +36,12 @@ const Navbar = () => {
 export default Navbar;
 
 const MenuList = ({ data }) => {
+  // const history = useHistory();
+
+  const onClickMenu = ({ path }) => {
+    window.location.assign(path);
+  };
+
   return (
     <>
       <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium lg:flex-row list-none lg:ml-auto md:font-medium">
@@ -42,7 +50,7 @@ const MenuList = ({ data }) => {
             <li className="nav-item" key={item.id}>
               <a
                 className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                href={item.path}>
+                href={() => onClickMenu({ path: item.path })}>
                 <span className="ml-2">{item.label}</span>
               </a>
             </li>
